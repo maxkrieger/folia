e676886a9026a80e3165329b63e1b40732e3c464
maxkrieger <themaxaxis@gmail.com>
2019-11-21 03:03:22 -0500

    Updates

---
+++ precache-manifest.67e2d6f99ad7a2a9ee1cba94ba93522e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdb74803f338879d21f2bb30a30cb8e",
+    "url": "/folia/index.html"
+  },
+  {
+    "revision": "bd3b90e789ded000d1ad",
+    "url": "/folia/static/css/main.4c7dea7e.chunk.css"
+  },
+  {
+    "revision": "af68d84d2f5c35c4b08a",
+    "url": "/folia/static/js/2.1af3bd74.chunk.js"
+  },
+  {
+    "revision": "bd3b90e789ded000d1ad",
+    "url": "/folia/static/js/main.51872632.chunk.js"
+  },
+  {
+    "revision": "4e7218323008604c6b50",
+    "url": "/folia/static/js/runtime-main.aa1fcc91.js"
+  },
+  {
+    "revision": "c5498b74a331dd71bd60e462a7557a17",
+    "url": "/folia/static/media/turtle.c5498b74.svg"
+  }
+]);
